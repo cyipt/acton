c8b1e38ee15ee80dbc157191bfade5102a152e17
Robin Lovelace <rob00x@gmail.com>
2020-01-07 16:31:30 +0000

    get wpz from url - reproducibility

--- code/workzones.R
+++ code/workzones.R
@@ -3,7 +3,9 @@ library(mapview)
 library(pct)
 library(tidyverse)
 
-wz = st_read("../NewDevelopmentsCycling/data/WorkplaceZonesFullExtent/Workplace_Zones_December_2011_Full_Extent_Boundaries_in_England_and_Wales.shp") %>% st_transform(27700)
+# wz = st_read("../NewDevelopmentsCycling/data/WorkplaceZonesFullExtent/Workplace_Zones_December_2011_Full_Extent_Boundaries_in_England_and_Wales.shp") %>% st_transform(27700)
+
+wz = read_sf("http://geoportal1-ons.opendata.arcgis.com/datasets/a399c2a5922a4beaa080de63c0a218a3_1.geojson")
 
 reszone = pct::get_pct_zones(region = "west-yorkshire", geography = "lsoa", purpose = "commute") %>% st_transform(27700)
 iow = pct::get_pct_zones(region = "isle-of-wight", geography = "msoa", purpose = "commute") %>% st_transform(27700)
