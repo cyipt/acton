6072c19991562f0cd85530d28020f78d6bb6ef40
Robin Lovelace <rob00x@gmail.com>
2020-01-07 16:59:09 +0000

    update workzones.R code

--- code/workzones.R
+++ code/workzones.R
@@ -14,21 +14,35 @@ wz1 = wz[1, ]
 mapview(wz1) # quite detailed...
 wz1_simplified = rmapshaper::ms_simplify(wz1, sys = T)
 mapview(wz1_simplified) # quite detailed...
-system.time({wz_5perc_simplified = rmapshaper::ms_simplify(wz %>% sample_frac(0.05), sys = T)})
-system.time({wz_simplified = rmapshaper::ms_simplify(wz, sys = T)})
 
+# test run on sample of data to estimate runtime
+system.time({wz_5perc_simplified = rmapshaper::ms_simplify(wz %>% sample_frac(0.05), sys = T)})
 saveRDS(wz_5perc_simplified, "wz_5perc_simplified.Rds")
 file.size("wz_5perc_simplified.Rds") / 1e6
 piggyback::pb_upload("wz_5perc_simplified.Rds")
 
-reszone = pct::get_pct_zones(region = "west-yorkshire", geography = "lsoa", purpose = "commute") %>% st_transform(27700)
-iow = pct::get_pct_zones(region = "isle-of-wight", geography = "msoa", purpose = "commute") %>% st_transform(27700)
+# run on whole dataset
+system.time({wz_simplified = rmapshaper::ms_simplify(wz, sys = T)})
+saveRDS(wz_simplified, "wz_simplified.Rds")
+piggyback::pb_upload("wz_simplified.Rds")
+names(wz_simplified)
+
+reszone = pct::get_pct_zones(region = "west-yorkshire", geography = "lsoa", purpose = "commute") # %>% st_transform(27700)
+iow = pct::get_pct_zones(region = "isle-of-wight", geography = "msoa", purpose = "commute") # %>% st_transform(27700)
 
 # wz_wyorks = st_intersection(wz,reszone) # this displays both x and y, with lots of splices
 # wz_wyorks = st_crosses(wz,reszone) # this produces error 'Error in (function (classes, fdef, mtable)  : unable to find an inherited method for function ‘mapView’ for signature ‘"sgbp"’
-wz_wyorks = st_intersection(wz,st_buffer(reszone,1000))
+# wz_wyorks = st_intersection(wz,st_buffer(reszone,1000))
+wz_centroids = st_centroid(wz_simplified)
+st_crs(wz_simplified)
+st_crs(reszone)
+wz_centroids_wy = wz_centroids[reszone, ]
+mapview(wz_centroids_wy)
+# wz_simplified_wy = wz_simplified[wz_centroids_wy,] # failed, missing areas
+wz_simplified_wy = wz_simplified %>% filter(wz11cd %in% wz_centroids$wz11cd)
+mapview(wz_simplified_wy)
 
-wz_simple = st_simplify(wz_wyorks,preserveTopology = TRUE, dTolerance = 10)
+# wz_simple = st_simplify(wz_wyorks,preserveTopology = TRUE, dTolerance = 10) # use rmapshaper
 
 rz = st_boundary(reszone)
 
