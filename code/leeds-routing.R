0813d262bd13c7b8e08fb1d057851e4a69315233
joeytalbot <joeytalbot85@gmail.com>
2020-01-27 18:05:08 +0000

    MSOA and LSOA routing

--- code/leeds-routing.R
+++ code/leeds-routing.R
@@ -20,19 +20,25 @@ od_all_en_wales = pct::get_od()
 c = pct::get_pct_centroids(region = "west-yorkshire", geography = "lsoa") %>% rename(LA_Code = lad11cd)
 m = pct::get_pct_centroids(region = "west-yorkshire", geography = "msoa") %>% rename(LA_Code = lad11cd)
 lines_pct_lsoa = pct::get_pct_lines(region = "west-yorkshire", geography = "lsoa")
-lines_pct_msoa = pct::get_pct_lines(region = "west-yorkshire", geography = "msoa")
+# lines_pct_msoa = pct::get_pct_lines(region = "west-yorkshire", geography = "msoa")
+
+
+# MSOA data ---------------------------------------------------------------
+
+#this can find all OD pairs (even beyond west yorkshire and >20km away)
 
 centroids_nearest_site = sites$msoa_code
-#uses MSOA codes
+
+# need to also add in lines where geo_code2 matches the sites
 od_from_centroids_near_sites = od_all_en_wales %>%
   filter(geo_code1 %in% centroids_nearest_site)
 
-od_from_centroids_near_sites = lines_pct_msoa %>%
-  select(geo_code1, geo_code2, all, bicycle, car_driver, car_passenger
-         # bus, taxi etc
-         ) %>%
-  filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% m$geo_code) %>%
-  sf::st_drop_geometry()
+# od_from_centroids_near_sites = lines_pct_msoa %>%
+#   select(geo_code1, geo_code2, all, bicycle, car_driver, car_passenger
+#          # bus, taxi etc
+#          ) %>%
+#   filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% m$geo_code) %>%
+#   sf::st_drop_geometry()
 
 # # add on the site_id (first column)
 sites_column_name_updated = sites %>%
@@ -42,10 +48,38 @@ lines_to_sites = stplanr::od2line(od_from_centroids_near_sites, sites_column_nam
 plot(lines_to_sites)
 mapview::mapview(lines_to_sites)
 
+
+# LSOA data ---------------------------------------------------------------
+
+##this has the limitations that only sites from west yorkshire, with journeys of less than 20km are selected
+
+centroids_nearest_site = sites$geo_code
+
+od_from_centroids_near_sites = lines_pct_lsoa %>%
+  select(geo_code1, geo_code2, all, bicycle, car_driver, car_passenger
+         # bus, taxi etc
+  ) %>%
+  filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% c$geo_code) %>%
+  sf::st_drop_geometry()
+
+# # add on the site_id (first column)
+sites_column_name_updated = sites %>%
+  select(geo_code, everything())
+
+lines_to_sites = stplanr::od2line(od_from_centroids_near_sites, sites_column_name_updated, c)
+plot(lines_to_sites)
+mapview::mapview(lines_to_sites)
+
+
+# Creating routes ---------------------------------------------------------
+
 library(parallel)
 cl <- makeCluster(detectCores())
-clusterExport(cl, c("journey"))
+# clusterExport(cl, c("journey"))#not needed and not working
 routes_to_site = route(l = lines_to_sites, route_fun = cyclestreets::journey, cl = cl)
+
+#There is an error with the quiet routes. These come out as identical to the fast routes
 routes_to_site_quietest = route(l = lines_to_sites, route_fun = cyclestreets::journey, cl = cl, plan = "quietest")
-mapview::mapview(routes_to_site_quietest)
+
+mapview::mapview(routes_to_site)
 identical(routes_to_site, routes_to_site_quietest)
