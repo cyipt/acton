9fe6a80d453ec22bf30667e81f0263a4f9ec3151
joeytalbot <joeytalbot85@gmail.com>
2020-02-06 17:06:50 +0000

    Busyness maps for case study

--- code/leeds-routing.R
+++ code/leeds-routing.R
@@ -13,7 +13,7 @@ library(stplanr)
 sites = sf::read_sf("https://github.com/cyipt/acton/releases/download/0.0.1/sites-leeds.geojson")
 # identify centroid closest to
 
-od_all_en_wales = pct::get_od()
+od_all_en_wales = pct::get_od(omit_intrazonal = TRUE)
 
 # Create desire lines from development site to common destinations from nearest centroid
 
@@ -31,20 +31,20 @@ centroids_nearest_site = sites$msoa_code
 
 # need to also add in lines where geo_code2 matches the sites
 od_from_centroids_near_sites = od_all_en_wales %>%
-  filter(geo_code1 %in% centroids_nearest_site)
+  filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% m$geo_code) %>%
+  sf::st_drop_geometry()
 
 # od_from_centroids_near_sites = lines_pct_msoa %>%
 #   select(geo_code1, geo_code2, all, bicycle, car_driver, car_passenger
 #          # bus, taxi etc
 #          ) %>%
-#   filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% m$geo_code) %>%
-#   sf::st_drop_geometry()
+#   filter(geo_code1 %in% centroids_nearest_site, geo_code2 %in% m$geo_code)
 
 # # add on the site_id (first column)
 sites_column_name_updated = sites %>%
   select(msoa_code, geo_code, everything())
 
-lines_to_sites = stplanr::od2line(od_from_centroids_near_sites, sites_column_name_updated, m)
+lines_to_sites = stplanr::od2line(flow = od_from_centroids_near_sites,m,sites_column_name_updated)#why doesn't this work?
 plot(lines_to_sites)
 mapview::mapview(lines_to_sites)
 
@@ -84,3 +84,21 @@ routes_to_site_quietest = route(l = lines_to_sites, route_fun = cyclestreets::jo
 
 mapview::mapview(routes_to_site)
 identical(routes_to_site, routes_to_site_quietest)
+
+
+
+# Busyness ----------------------------------------------------------------
+
+routes_to_site$busyness = routes_to_site$busynance / routes_to_site$distance
+
+mapview(routes_to_site["busyness"],lwd = routes_to_site$all, scale = 0.01) + mapview(sites_column_name_updated[1,])
+
+s1 = unique(routes_to_site$geo_code1)[1]
+s2 = unique(routes_to_site$geo_code1)[2]
+s3 = unique(routes_to_site$geo_code1)[3]
+s4 = unique(routes_to_site$geo_code1)[4]
+
+tmap_mode("view")
+tm_shape(routes_to_site[routes_to_site$geo_code1 == s4,]) +
+  tm_lines(col = "busyness", palette = "YlOrRd", contrast = c(0.3, 0.9), lwd = "all", scale = 5)
+
